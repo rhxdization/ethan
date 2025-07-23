8d2149e84bf0a87a37e7b11a0cc2b87e8adad89d
rhx <wongethan120@gmail.com>
2025-07-23 07:59:31 +0000

    Jul 23, 2025, 7:59 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -39,7 +39,7 @@ function draw() {
   if (ball.x < 0 + ball.diameter / 2 || ball.x > width - ball.diameter / 2) {
     ball.vel.x *= -1;
   }
-  if (ball.x < 0 + ball.diameter / 2 || ball.x > width - ball.diameter / 2) {
+  if (ball.y < 0 + ball.diameter / 2 || ball.y > height - ball.diameter / 2) {
     ball.vel.x *= -1;
   }
 }
