215b24a753fbd95b03dba9300e9b4f9a208bf5d2
rhx <wongethan120@gmail.com>
2025-07-23 08:50:23 +0000

    Jul 23, 2025, 8:50 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -1,4 +1,4 @@
-function setup() {
+
   // write your codes here
 
   // --- Exercise: Area of Triangle ---
@@ -15,5 +15,17 @@ function setup() {
 
   // --- Exercise: Array operations (groceries) ---
   // write your codes here
+  function setup() {
+    new Canvas(800,400);
+    let a = 5;
+    let b = 10;
+
+    let sum = (a + b);
+    console.log(sum);
+    console.log(a*b);
+
+    fill(255,0,0);
+    textSize(14);
+    text("The sum of a and b is " + sum, 50, 50)
 }
 
