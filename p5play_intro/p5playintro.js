2e585122ce75b75936f8356cbca98426209018ff
rhx <wongethan120@gmail.com>
2025-07-23 08:45:56 +0000

    Jul 23, 2025, 8:45 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -50,8 +50,8 @@ function draw() {
     ball.vel.y *= -1;
   }
 
-  // box.x = mouseX;
-  // box.y = mouseY;
+  box.x = mouseX;
+  box.y = mouseY;
 
   if (mouse.presses("left")) {
     let x = new Sprite (mouseX, mouseY, 50, 50)
