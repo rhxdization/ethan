0ed01814bc219f94cbf9d9147ca6ebc97146d84d
rhx <wongethan120@gmail.com>
2025-07-23 08:49:38 +0000

    Jul 23, 2025, 8:49 AM

--- p5play_intro/basic2.js
+++ p5play_intro/basic2.js
@@ -1,11 +1,12 @@
 function setup() {
-    new Canvas(800,400)
-    let a = 5
-    let b = 10
+    new Canvas(800,400);
+    let a = 5;
+    let b = 10;
 
     let sum = (a + b);
     console.log(sum);
     console.log(a*b);
 
-    fill(255,0,0)
+    fill(255,0,0);
+    textSize()
 }
