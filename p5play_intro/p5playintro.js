d92dc8e200df31ea89dd6ff65e370d65c4824d15
rhx <wongethan120@gmail.com>
2025-07-30 07:37:53 +0000

    Jul 30, 2025, 7:37 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -2,7 +2,7 @@ function setup() {
   // Set up the canvas
   new Canvas(1200, 500);
   background(255); //background color
-  world.gravity.y = 8;
+  world.gravity.y = 20;
 
   ball = new Sprite();
   ball.x = 100;
