788a0d63a2fc98ed52eca6c2b1c27db578a48e30
rhx <wongethan120@gmail.com>
2025-07-30 08:04:37 +0000

    Jul 30, 2025, 8:04 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -51,7 +51,9 @@ function setup() {
     // for (let i = 20; i>=1; i--) {
     //   console.log(i);
     // }
+    
+    
     for (let i = 2; i<=20; i+=2) {
-      
+      console.log(i);
     }
   }
