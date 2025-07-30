526eedde445591adf22cf028c94b08118e312a2c
rhx <wongethan120@gmail.com>
2025-07-30 07:47:13 +0000

    Jul 30, 2025, 7:47 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -16,26 +16,28 @@
   // --- Exercise: Array operations (groceries) ---
   // write your codes here
 function setup() {
-    new Canvas(800,400);
-    // let a = 5;
-    // let b = 10;
+    // new Canvas(800,400);
+    // // let a = 5;
+    // // let b = 10;
 
-    // let sum = (a + b);
-    // console.log(sum);
-    // let product = (a * b);
+    // // let sum = (a + b);
+    // // console.log(sum);
+    // // let product = (a * b);
 
-    fill(255,0,0);
-    textSize(60);
-    // text("The sum of a and b is " + sum, 50, 50);
-    // text("The product of a and b is " + product, 50, 70);
+    // fill(255,0,0);
+    // textSize(60);
+    // // text("The sum of a and b is " + sum, 50, 50);
+    // // text("The product of a and b is " + product, 50, 70);
 
-    let base = 10;
-    let height = 5;
-    let area = 10*5/2;
-    text("The triangles area is " + area, 50, 50);
-    text("The triangles area is " + area, 50, 100);
-    text("The triangles area is " + area, 50, 150);
-    text("The triangles area is " + area, 50, 200);
-    text("The triangles area is " + area, 50, 250);
-    text("The triangles area is " + area, 50, 300);
+    // let base = 10;
+    // let height = 5;
+    // let area = 10*5/2;
+    // text("The triangles area is " + area, 50, 50);
+    // text("The triangles area is " + area, 50, 100);
+    // text("The triangles area is " + area, 50, 150);
+    // text("The triangles area is " + area, 50, 200);
+    // text("The triangles area is " + area, 50, 250);
+    // text("The triangles area is " + area, 50, 300);
+
+    
 }
