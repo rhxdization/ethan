f61deae7f1a6ead10176102c7f9487a3a47e35e5
rhx <wongethan120@gmail.com>
2025-08-07 09:45:10 +0000

    Aug 7, 2025, 9:45 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -18,7 +18,7 @@ function setup() {
     background("skyblue");
     let floor = new Sprite();
     floor.img = base;
-    floor.y = 300;
+    floor.y = 600;
 }
 
 // called repeatedly 60 times in a second
