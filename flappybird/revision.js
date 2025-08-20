d1740398db46e2738850490674a01a576476168c
rhx <wongethan120@gmail.com>
2025-08-20 08:57:52 +0000

    Aug 20, 2025, 8:57 AM

--- flappybird/revision.js
+++ flappybird/revision.js
@@ -12,7 +12,7 @@ let i = 1
 // let n = ["1", "2", "3", "4"];
 
 function preload() {
-    let img1 = loadImage("assets/igm.jpeg"); 
+    let image = loadImage("assets/igm.jpeg"); 
 }
 
 function setup() {
@@ -35,6 +35,6 @@ bird.bounciness = .5;
     bird.mass = 2;
     bird.drag = 0.02;
     bird.collider = "dynamic";
-    bird.img = img1;
+    bird.img = image;
 }
 
