46b0f5aaa34ad218080c23aa0477081f3029f70f
rhx <wongethan120@gmail.com>
2025-08-20 08:59:00 +0000

    Aug 20, 2025, 8:59 AM

--- flappybird/revision.js
+++ flappybird/revision.js
@@ -12,7 +12,7 @@ let i = 1
 // let n = ["1", "2", "3", "4"];
 
 function preload() {
-    image1 = loadImage("assets/igm.jpeg"); 
+    image1 = loadImage("assets/vxfb,nckml mcvmbvkmlv"); 
 }
 
 function setup() {
