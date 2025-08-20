f3fc645e2d4a21479391effc7ea7d4f2b63c0d19
rhx <wongethan120@gmail.com>
2025-08-20 08:55:41 +0000

    Aug 20, 2025, 8:55 AM

--- flappybird/revision.js
+++ flappybird/revision.js
@@ -3,7 +3,8 @@
 //declare a variable
 let i = 1
 
-let img1 = // let twominisandabigpot = "sixsevenmustardmangos";
+let img1 = "flappybird/assets/igm.jpeg" 
+// let twominisandabigpot = "sixsevenmustardmangos";
 // let peepee = 67
 // let pie = 3.142
 
