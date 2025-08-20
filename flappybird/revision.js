b1238027b739ef299fbe2732837e25a5e634b497
rhx <wongethan120@gmail.com>
2025-08-20 08:35:59 +0000

    Aug 20, 2025, 8:35 AM

--- flappybird/revision.js
+++ flappybird/revision.js
@@ -7,10 +7,10 @@
 // let pie = 3.142
 
 function setup() {
-    for (let i = 1; i < 4; i++) {
-        console.log(i);
-    }
-    while (i < 4) {
+    // for (let i = 1; i < 4; i++) {
+    //     console.log(i);
+    // }
+    while (i) {
         console.log(i)
     }
 }
