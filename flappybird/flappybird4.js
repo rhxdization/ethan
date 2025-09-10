33220fdd10b8544bee76297336191ff14786c3fb
rhx <wongethan120@gmail.com>
2025-09-10 08:59:02 +0000

    Sep 10, 2025, 8:59 AM

--- flappybird/flappybird4.js
+++ flappybird/flappybird4.js
@@ -74,7 +74,9 @@ function draw() {
     
     image(bg, 0, 0, width, height)
 
-    if (kb.presses('space') || kb.presses('w'))
+    if (kb.presses('space') || kb.presses('w') || mouse.presses('left')) {
+        
+    }
 
     if (kb.presses("space") || mouse.presses("left") || kb.presses("w")) {
         bird.vel.y = -7;
