7c289749c3219681dfcaf90d792daccf03a84504
rhx <wongethan120@gmail.com>
2025-09-10 08:49:59 +0000

    Sep 10, 2025, 8:49 AM

--- flappybird/flappybird4.js
+++ flappybird/flappybird4.js
@@ -59,7 +59,7 @@ function setup() {
     pipeGroup = new Group();
 
     ssl = new Sprite(width/2, height/2, 50, 50, 'none');
-    ssl.x = ss
+    ssl = ss
 }
 
 // called repeatedly 60 times in a second
