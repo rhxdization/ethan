a5e9be80c8e9915ac2fbbbcf1ed713f1390ab373
rhx <wongethan120@gmail.com>
2025-09-10 08:01:23 +0000

    Sep 10, 2025, 8:01 AM

--- flappybird/flappybird4.js
+++ flappybird/flappybird4.js
@@ -106,7 +106,7 @@ function spawnPipePair() {
     let midY = height/2;
     let gap = 50;
 
-    bottomPipe = new  Sprite(bird.x + 400, midY + random(100,300) + gap/2 + 200, 52, 320, 'static');
+    bottomPipe = new  Sprite(bird.x + 400, midY + random(-100,100) + gap/2 + 200, 52, 320, 'static');
     bottomPipe.img = pipe;
 
     topPipe = new Sprite(bird.x + 400, midY - random(100,300) - gap/2 - 200, 52, 320, 'static');
