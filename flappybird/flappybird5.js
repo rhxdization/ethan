834ba576ea8aa25b4e67c69e923bb4a5d2c57e6b
rhx <wongethan120@gmail.com>
2025-09-17 08:48:08 +0000

    Sep 17, 2025, 8:48 AM

--- flappybird/flappybird5.js
+++ flappybird/flappybird5.js
@@ -175,7 +175,7 @@ function drawScore(x, y, score, digitWidth, digitHeight) {
         let xPos = startX + i * digitWidth;
         let digitSprite = new scoreDigits.Sprite(xPos, y, digitWidth, digitHeight);
         digitSprite.img = numberImages[digit];
- 
+        moveGroup(scoreDigits,)
     }
 }
 
