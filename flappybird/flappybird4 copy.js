40945e0391a901ca3b556e23a9dab30383647b1e
rhx <wongethan120@gmail.com>
2025-09-17 08:04:56 +0000

    Sep 17, 2025, 8:04 AM

---
+++ flappybird/flappybird4 copy.js
@@ -0,0 +1,151 @@
+// the final game
+
+// write your codes here
+
+let bird, floor;
+let bg, base;
+let flapMidImg, flapDownImg, flapUpImg;
+let pipeGroup;
+let pipe;
+let bottomPipe, topPipe;
+let gameover;
+let gameoverlabel;
+let ssi;
+let ssl;
+let start = false;
+
+// load media files like img or sound effects
+
+function preload() {
+    bg = loadImage("assets/background-day.png");
+    base = loadImage("assets/base.png");
+
+    flapMidImg = loadImage("assets/yellowbird-midflap.png");
+    flapDownImg = loadImage("assets/yellowbird-downflap.png");
+    flapUpImg = loadImage("assets/yellowbird-upflap.png");
+
+    pipe = loadImage('assets/pipe-green.png');  
+
+    gameover = loadImage('assets/gameover.png');
+    ssi = loadImage('assets/message.png');
+}
+
+// similiar to "when green flag clicked"
+// run once
+
+function setup() {
+    
+    new Canvas(400,600);
+    background("skyblue");
+    
+    floor = new Sprite();
+    floor.collider = "static";
+    floor.img = base;
+    floor.y = height - 20;
+    floor.height = 118;
+    floor.width = 400;
+    
+    bird = new Sprite();
+    bird.img = flapMidImg;
+    bird.bounciness = .5;
+    bird.y = 200;
+    bird.width = 30;
+    bird.height = 30;
+    bird.mass = 2;
+    bird.drag = 0.02;
+    bird.collider = "dynamic";
+
+    world.gravity.y = 20;
+
+    pipeGroup = new Group();
+
+    ssl = new Sprite(width/2, height/2, 50, 50, 'none');
+    ssl.img = ssi;
+}
+
+// called repeatedly 60 times in a second
+// where interaction and animation
+
+function draw() {
+    
+    image(bg, 0, 0, width, height)
+
+    if (kb.presses('space') || kb.presses('w') || mouse.presses('left')) {
+        start = true;
+        startScreenLabel.visible = false;
+    }
+
+    if (start) {
+            
+    if (frameCount === 1) {
+        spawnPipePair();
+    }
+
+    if (kb.presses("space") || mouse.presses("left") || kb.presses("w")) {
+        bird.vel.y = -7;
+        bird.sleeping = false;
+    }
+
+    fill("black");
+    textSize = 14;
+    text("vel.y: " + bird.vel.y.toFixed(0), 10, 20);
+    text("is moving: " + bird.isMoving, 10, 40);
+    text("is sleeping: " + bird.sleeping, 10, 60);
+
+    if (bird.vel.y < 0.5) {
+        bird.img = flapDownImg;
+        bird.rotation = -30;
+    }
+    else if (bird.vel.y > 0.5) {
+        bird.img = flapUpImg;
+        bird.rotation = 30;
+    }
+    else {
+        bird.img = flapMidImg;
+        bird.rotation = 0;
+    }
+    
+    bird.x += 3;
+    camera.x = bird.x;
+    floor.x = bird.x;
+
+    if(frameCount % 90 === 0) {
+        spawnPipePair();
+    }
+
+    for (let pipe of pipeGroup){
+        if (pipe.x < -50){
+            pipe.remove();
+        }
+    }
+
+    if (bird.collides(pipeGroup) || bird.collides(floor) || bird.y === 0) {
+        gameoverlabel = new Sprite(width/2, height/2, 192, 42, 'static');
+        gameoverlabel.img = gameover;
+        gameoverlabel.layer = 100;
+        gameoverlabel.x = camera.x;
+        noLoop();
+    } 
+    
+    }
+
+
+}
+
+function spawnPipePair() {
+    let midY = (height+random(-150,150))/2;
+    let gap = 50;
+
+    bottomPipe = new  Sprite(bird.x + 400, midY + gap/2 + 200, 52, 320, 'static');
+    bottomPipe.img = pipe;
+
+    topPipe = new Sprite(bird.x + 400, midY - gap/2 - 200, 52, 320, 'static');
+    topPipe.img = pipe;
+    topPipe.rotation = -180;
+
+    pipeGroup.add(bottomPipe)
+    pipeGroup.add(topPipe);
+    pipeGroup.layer = 0;
+
+}
+
