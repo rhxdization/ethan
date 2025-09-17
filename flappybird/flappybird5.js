b2263fa380a0fef0dfebaa785049daaa2b30cdcc
rhx <wongethan120@gmail.com>
2025-09-17 08:51:09 +0000

    Sep 17, 2025, 8:51 AM

--- flappybird/flappybird5.js
+++ flappybird/flappybird5.js
@@ -181,5 +181,5 @@ function drawScore(x, y, score, digitWidth, digitHeight) {
 
 function moveGroup(group, targetX, spacing) {
     let totalWidth = (group.length - 1) * spacing
-    
+    let startX = (targetX - totalWidth/2)
 }
