845e8535e0de43996cbe84a4111d3ff97944cea6
rhx <wongethan120@gmail.com>
2025-09-24 09:00:16 +0000

    Sep 24, 2025, 9:00 AM

--- flappybird/flappybird6.js
+++ flappybird/flappybird6.js
@@ -164,7 +164,7 @@ function draw() {
         setTimeout(() => {
             score = 0;
             startGame = false;
-            pipe.removeAll();
+            pipeGroup.removeAll();
             bird.vel.x = 0;
             bird.vel.y - 0;
             bird.rotation = 0;
