e4320ed9b193684ad1265b2c831794b8d888e110
rhx <wongethan120@gmail.com>
2025-09-24 08:00:21 +0000

    Sep 24, 2025, 8:00 AM

--- flappybird/flappybird6.js
+++ flappybird/flappybird6.js
@@ -146,7 +146,7 @@ function draw() {
 
         for (let pipe of pipeGroup) {
             let pipeRightEdge = pipe.x + pipe.w / 2;
-            let birdLeftEdge
+            let birdLeftEdge = bird.x
         }
 
     }
