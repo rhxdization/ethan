d37669acf625e839bb5030b3806925efc432901e
rhx <wongethan120@gmail.com>
2025-10-01 08:19:37 +0000

    Oct 1, 2025, 8:19 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -11,7 +11,7 @@ function preload() {
     let watermelon = {
         whole: loadImage('assets/watermelonwhole.png')
     }
-    let fruitTypes
+    let fruitTypes = [peach, watermelon];
 }
 
 function setup() {
