ca230ed926fdb6d9b4dd604dd484be02b1065af3
rhx <wongethan120@gmail.com>
2025-10-01 08:25:52 +0000

    Oct 1, 2025, 8:25 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -29,5 +29,6 @@ function draw() {
 function spawnFruit() {
     let fruitData = random(fruitTypes);
     let randomX = random(300,500);
-    let fruit = new fruitGroup.Sprite(randomX)
+    let fruit = new fruitGroup.Sprite(randomX, height+20, 40);
+    fruit.image
 }
