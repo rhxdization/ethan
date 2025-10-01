df169e3cc2c6213c4eee70f1856f7f1912b5f889
rhx <wongethan120@gmail.com>
2025-10-01 07:53:32 +0000

    Oct 1, 2025, 7:53 AM

--- flappybird/flappybird6.js
+++ flappybird/flappybird6.js
@@ -104,7 +104,7 @@ function draw() {
         }
 
         if (kb.presses("space") || mouse.presses() || kb.presses("w")) {
-            bird.vel.y = -25;
+            bird.vel.y = -6;
             bird.sleeping = false;
             flapSound.play();
         }
